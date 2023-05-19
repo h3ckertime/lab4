bba2abe62e429845f716f2524dd67a44b086f18d
Sahmeran <125767330+h3ckertime@users.noreply.github.com>
2023-05-19 09:25:51 +0400

    Add files via upload

---
+++ float.py
@@ -0,0 +1,10 @@
+# floating tipində dəyişənlərin təyini
+a = 2.5
+b = 1.2
+
+# ondalıqlı ədədlərin toplanması
+c = a + b
+print(c)  
+
+# nəticə: 3.7
+
---
+++ integer.py
@@ -0,0 +1,10 @@
+#integer tipli dəyişənlərin təyini
+a = 5
+b = 10 
+
+#onlar üzərində toplama əməli
+c = a + b
+print(c)
+
+#nəticə 15
+
